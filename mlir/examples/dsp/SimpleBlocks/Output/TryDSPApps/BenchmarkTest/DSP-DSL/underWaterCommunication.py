7ba459471806027efcfd56a092f9bc0bba7576c0
Atharva Khedkar <atharva.khedkar123@gmail.com>
2024-11-09 19:20:58 -0700

    update uwc results

--- mlir/examples/dsp/SimpleBlocks/Output/TryDSPApps/BenchmarkTest/DSP-DSL/underWaterCommunication.py
+++ mlir/examples/dsp/SimpleBlocks/Output/TryDSPApps/BenchmarkTest/DSP-DSL/underWaterCommunication.py
@@ -2,7 +2,7 @@ def main() {
   var fs = 1000;
   # var step = 1/fs; 
   # print(step);
-	var input = getRangeOfVector(0, 100000000, 1);
+	var input = getRangeOfVector(0, 100000000, 0.000125);
   var pi = 3.14159265359;
   var getMultiplier = 2 * pi * 5;
   # print(getMultiplier);
